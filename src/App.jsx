44ab81c24a20b0f673092101af8594c210adaffe
Kagensyou12 <hhadisurya01@gmail.com>
2024-07-27 00:24:41 +0700

    improving the front end and adding my photo

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,31 @@ function App() {
     <div className="App">
       <header className='header'>
         <div className='btn-head'>
-        <button>Home</button>
-        <button>Projects</button>
-        <button>About Me</button>
-        <button>Contact Me</button>
+          <button>Home</button>
+          <button>Projects</button>
+          <button>About Me</button>
+          <button>Contact Me</button>
         </div>
       </header>
       <main className='body'>
         <div className='p1'>
-       <h1>Hi, I'm Hadisurya</h1>
-      <p>
-        I am a passionate Software Engineer and an undergraduate student specializing in Computer Science at Bina Nusantara University. With a strong enthusiasm for technology, I am dedicated to continuous learning and staying updated with the latest advancements. 
-      </p>
-      <p>
-       My key strengths include problem-solving and rapid learning, which enable me to tackle complex challenges and adapt to new technologies quickly. I thrive in dynamic environments where innovation and creativity are valued.
-      </p>
-      <p>
-        I am excited about the opportunity to contribute my skills and knowledge to meaningful projects and collaborate with others to achieve great outcomes.
-    </p>
-
-       </div>
+          <div className="text-content">
+            <h1>Hi, I'm Hadisurya</h1>
+            <p>
+              I am a passionate Software Engineer and an undergraduate student specializing in Computer Science at Bina Nusantara University. With a strong enthusiasm for technology, I am dedicated to continuous learning and staying updated with the latest advancements.
+            </p>
+            <p>
+              My key strengths include problem-solving and rapid learning, which enable me to tackle complex challenges and adapt to new technologies quickly. I thrive in dynamic environments where innovation and creativity are valued.
+            </p>
+            <p>
+              I am excited about the opportunity to contribute my skills and knowledge to meaningful projects and collaborate with others to achieve great outcomes.
+            </p>
+          </div>
+          
+        </div>
+        <div className="image-container">
+            <img src="\Portofolio.jpg" alt="Portofolio" className="circular-image" />
+          </div>
       </main>
       <footer className='footer'>
         <h1>This is the footer section</h1>
