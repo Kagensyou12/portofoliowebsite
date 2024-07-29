ec844a0a3c2833d3ab6fbff9bc91f42b0884832f
Kagensyou12 <hhadisurya01@gmail.com>
2024-07-30 02:25:39 +0700

    changes to About me and work for tommorow

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,21 +14,27 @@ function AboutMe() {
                         My academic journey has been both challenging and rewarding, providing me with a solid foundation in various areas of computing. 
                         I have a strong passion for web development, particularly using React, which I find incredibly powerful for building dynamic and interactive user interfaces. 
                         Over the past few years, I have honed my skills by working on numerous projects, both independently and as part of a team. </p>
-
                     <p>Beyond web development, I am deeply interested in exploring other programming domains. 
                     Mobile programming has caught my attention, as I see it as a crucial skill in our increasingly mobile-first world.
                     I'm also fascinated by game development, which combines creativity with technical expertise to create immersive experiences. 
                     </p> 
-
                     <p>My curiosity doesn't stop there; I am always eager to learn new languages and frameworks to expand my versatility as a developer.
                     In addition to my technical pursuits, I enjoy collaborating with like-minded individuals and working with said individuals. 
                     These experiences have not only sharpened my problem-solving skills but also taught me the importance of teamwork and communication.
                     </p> 
-                    
                     <p>I believe that continuous learning and adaptability are key in the ever-evolving field of technology, and I am committed to staying updated with the latest trends and innovations.
                     My ultimate goal is to leverage my skills and knowledge to make a meaningful impact in the tech industry, contributing to projects that improve people's lives and push the boundaries of what's possible.
                     </p>
                 </div>
+            </div>
+{/* I have to work this out so that whenever i put on a new things onto the website it wouldnt change or effect the other things that i have build. such as this one the about me
+page is being push up instead of staying at the same place as i have designed it to. */}
+            <div className="experience">
+                <h1>This is where i want to put all of my experience</h1>
+            </div>
+
+            <div className="skills">
+                <h1>This is where i want to put all of my skills</h1>
             </div>
         </div>
     );
